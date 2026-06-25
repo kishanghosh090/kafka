5a6336b8dc052b8435cf63b90bafb7be8e89cea7
kishanghosh090 <kishanghosh090@gmail.com>
2026-06-26 03:01:33 +0530

    fix stream

---
+++ grpc/streamClient.ts
@@ -0,0 +1,28 @@
+import * as grpc from "@grpc/grpc-js";
+import protoloader from "@grpc/proto-loader";
+import path from "path";
+
+const dirname = import.meta.dirname;
+
+const PROTO_PATH = path.join(dirname, "greeter.proto");
+
+const packageDefination = protoloader.loadSync(PROTO_PATH);
+
+const proto = grpc.loadPackageDefinition(packageDefination).greeter as any; // here greeter is package name which is define in proto file greeter as `package greeter;`
+
+function main() {
+  const clinet = new proto.GreeterService(
+    "localhost:5050",
+    grpc.credentials.createInsecure(),
+  );
+ 
+  const call = clinet.getNumber({ count: 5 });
+
+    call.on("data", (res: any) => {
+      console.log(res);
+    });
+    call.on("end", () => {
+      console.log("stream eneded...");
+    });
+}
+main();
---
+++ grpc/streamServer.ts
@@ -0,0 +1,56 @@
+import * as grpc from "@grpc/grpc-js";
+import protoloader from "@grpc/proto-loader";
+import path from "path";
+
+const dirname = import.meta.dirname;
+
+const PROTO_PATH = path.join(dirname, "greeter.proto");
+
+const packageDefination = protoloader.loadSync(PROTO_PATH);
+
+const proto = grpc.loadPackageDefinition(packageDefination).greeter as any;
+
+function sayHello(call: any, callBack: any) {
+  // db call or other stuffs
+  const reply = { messgae: `hello, ${call.request.name}!` };
+  callBack(null, reply);
+}
+
+function getNumber(call: any) {
+  const count = call.request.count;
+
+  let current = 1;
+
+  const interval = setInterval(() => {
+    if (current > count) {
+      clearInterval(interval);
+      call.end();
+      return;
+    }
+    call.write({ order: current, number: current * 100 });
+    current++;
+  }, 1000);
+}
+
+function main() {
+  const server = new grpc.Server();
+
+  server.addService(proto!!.GreeterService!!.service!!, {
+    sayHello: sayHello,
+    getNumber: getNumber,
+  });
+  server.bindAsync(
+    "0.0.0.0:5050",
+    grpc.ServerCredentials.createInsecure(),
+    (err, port) => {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
+
+      console.log(`server is running at PORT ${port}`);
+    },
+  );
+}
+
+main();
